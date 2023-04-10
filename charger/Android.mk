P-404/android_vendor_404

vendor: Support charger on vendor
Change-Id: Id402ddeae8e43392d0a9f505e5cf4c162231d99f

charger/Android.mk
@@ -21,39 +21,70 @@ LOCAL_PATH := $(call my-dir)
 ifneq ($(strip $(LOCAL_CHARGER_NO_UI)),true)
 define _add-product-charger-image
 include $$(CLEAR_VARS)
+ifeq ($(2),vendor)
+LOCAL_MODULE := pixel_charger_res_images_charger_$(notdir $(1))_vendor
+else
 LOCAL_MODULE := pixel_charger_res_images_charger_$(notdir $(1))
+endif
 LOCAL_MODULE_STEM := $(notdir $(1))
+ifeq ($(2),vendor)
+_img_modules_vendor += $$(LOCAL_MODULE)
+else
 _img_modules += $$(LOCAL_MODULE)
+endif
 LOCAL_SRC_FILES := $1
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $$(TARGET_OUT_PRODUCT)/etc/res/images/charger
+LOCAL_MODULE_RELATIVE_PATH := res/images/charger
+ifeq ($(2),vendor)
+LOCAL_VENDOR_MODULE := true
+else
 LOCAL_PRODUCT_MODULE := true
+endif
 include $$(BUILD_PREBUILT)
 endef
 
 _img_modules :=
-_images :=
 $(foreach _img, $(call find-subdir-subdir-files, "images/charger", "*.png"), \
   $(eval $(call _add-product-charger-image,$(_img))))
+_img_modules_vendor :=
+$(foreach _img, $(call find-subdir-subdir-files, "images/charger", "*.png"), \
+  $(eval $(call _add-product-charger-image,$(_img),vendor)))
 
 ### pixel_charger_animation_file ###
 define _add-product-charger-animation-file
 include $$(CLEAR_VARS)
+ifeq ($(2),vendor)
+LOCAL_MODULE := pixel_charger_res_values_charger_$(notdir $(1))_vendor
+else
 LOCAL_MODULE := pixel_charger_res_values_charger_$(notdir $(1))
+endif
 LOCAL_MODULE_STEM := $(notdir $(1))
+ifeq ($(2),vendor)
+_anim_modules_vendor += $$(LOCAL_MODULE)
+else
 _anim_modules += $$(LOCAL_MODULE)
+endif
 LOCAL_SRC_FILES := $1
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $$(TARGET_OUT_PRODUCT)/etc/res/values/charger
+LOCAL_MODULE_RELATIVE_PATH := res/values/charger
+ifeq ($(2),vendor)
+LOCAL_VENDOR_MODULE := true
+LOCAL_OVERRIDES_MODULES := charger_res_images_vendor
+else
 LOCAL_PRODUCT_MODULE := true
+LOCAL_OVERRIDES_MODULES := charger_res_images
+endif
 include $$(BUILD_PREBUILT)
 endef
 
 _anim_modules :=
 $(foreach _txt, $(call find-subdir-subdir-files, "values/charger", "*.txt"), \
   $(eval $(call _add-product-charger-animation-file,$(_txt))))
+_anim_modules_vendor :=
+$(foreach _txt, $(call find-subdir-subdir-files, "values/charger", "*.txt"), \
+  $(eval $(call _add-product-charger-animation-file,$(_txt),vendor)))
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := p404_charger
@@ -61,8 +92,16 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_REQUIRED_MODULES := $(_img_modules) $(_anim_modules)
 include $(BUILD_PHONY_PACKAGE)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := p404_charger_vendor
+LOCAL_MODULE_TAGS := optional
+LOCAL_REQUIRED_MODULES := $(_img_modules_vendor) $(_anim_modules_vendor)
+include $(BUILD_PHONY_PACKAGE)
+
 _add-product-charger-image :=
 _add-product-charger-animation-file :=
 _img_modules :=
+_img_modules_vendor :=
 _anim_modules :=
+_anim_modules_vendor :=
 endif # LOCAL_CHARGER_NO_UI

MI439-CLO/android_vendorz_stop

charger/Android.mk
@@ -21,39 +21,70 @@ LOCAL_PATH := $(call my-dir)
 ifneq ($(strip $(LOCAL_CHARGER_NO_UI)),true)
 define _add-product-charger-image
 include $$(CLEAR_VARS)
+ifeq ($(2),vendor)
+LOCAL_MODULE := pixel_charger_res_images_charger_$(notdir $(1))_vendor
+else
 LOCAL_MODULE := pixel_charger_res_images_charger_$(notdir $(1))
+endif
 LOCAL_MODULE_STEM := $(notdir $(1))
+ifeq ($(2),vendor)
+_img_modules_vendor += $$(LOCAL_MODULE)
+else
 _img_modules += $$(LOCAL_MODULE)
+endif
 LOCAL_SRC_FILES := $1
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $$(TARGET_OUT_PRODUCT)/etc/res/images/charger
+LOCAL_MODULE_RELATIVE_PATH := res/images/charger
+ifeq ($(2),vendor)
+LOCAL_VENDOR_MODULE := true
+else
 LOCAL_PRODUCT_MODULE := true
+endif
 include $$(BUILD_PREBUILT)
 endef
 
 _img_modules :=
-_images :=
 $(foreach _img, $(call find-subdir-subdir-files, "images/charger", "*.png"), \
   $(eval $(call _add-product-charger-image,$(_img))))
+_img_modules_vendor :=
+$(foreach _img, $(call find-subdir-subdir-files, "images/charger", "*.png"), \
+  $(eval $(call _add-product-charger-image,$(_img),vendor)))
 
 ### pixel_charger_animation_file ###
 define _add-product-charger-animation-file
 include $$(CLEAR_VARS)
+ifeq ($(2),vendor)
+LOCAL_MODULE := pixel_charger_res_values_charger_$(notdir $(1))_vendor
+else
 LOCAL_MODULE := pixel_charger_res_values_charger_$(notdir $(1))
+endif
 LOCAL_MODULE_STEM := $(notdir $(1))
+ifeq ($(2),vendor)
+_anim_modules_vendor += $$(LOCAL_MODULE)
+else
 _anim_modules += $$(LOCAL_MODULE)
+endif
 LOCAL_SRC_FILES := $1
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $$(TARGET_OUT_PRODUCT)/etc/res/values/charger
+LOCAL_MODULE_RELATIVE_PATH := res/values/charger
+ifeq ($(2),vendor)
+LOCAL_VENDOR_MODULE := true
+LOCAL_OVERRIDES_MODULES := charger_res_images_vendor
+else
 LOCAL_PRODUCT_MODULE := true
+LOCAL_OVERRIDES_MODULES := charger_res_images
+endif
 include $$(BUILD_PREBUILT)
 endef
 
 _anim_modules :=
 $(foreach _txt, $(call find-subdir-subdir-files, "values/charger", "*.txt"), \
   $(eval $(call _add-product-charger-animation-file,$(_txt))))
+_anim_modules_vendor :=
+$(foreach _txt, $(call find-subdir-subdir-files, "values/charger", "*.txt"), \
+  $(eval $(call _add-product-charger-animation-file,$(_txt),vendor)))
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := aospa_charger
@@ -61,8 +92,16 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_REQUIRED_MODULES := $(_img_modules) $(_anim_modules)
 include $(BUILD_PHONY_PACKAGE)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := aospa_charger_vendor
+LOCAL_MODULE_TAGS := optional
+LOCAL_REQUIRED_MODULES := $(_img_modules_vendor) $(_anim_modules_vendor)
+include $(BUILD_PHONY_PACKAGE)
+
 _add-product-charger-image :=
 _add-product-charger-animation-file :=
 _img_modules :=
+_img_modules_vendor :=
 _anim_modules :=
+_anim_modules_vendor :=
 endif # LOCAL_CHARGER_NO_UI